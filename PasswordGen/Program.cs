ef899814ce2e2a2912bfdbc4fc1d9f5b3b56b298
Wentworth <terzoacclulle@hotmail.com>
2025-04-13 18:20:38 +0200

    fix do-while loop, avoid repetition, fix hardcoded character set.

--- PasswordGen/Program.cs
+++ PasswordGen/Program.cs
@@ -8,8 +8,6 @@ namespace PasswordGen
     {
         static void Main(string[] args)
         {
-
-            //Cool Ascii
             string title = @" ___                                  _ 
 | . \ ___  ___ ___ _ _ _  ___  _ _  _| |
 |  _/<_> |<_-<<_-<| | | |/ . \| '_>/ . |
@@ -19,62 +17,74 @@ namespace PasswordGen
 /  _>  ___ ._ _  ___  _ _  ___ _| |_ ___  _ _ 
 | <_/\/ ._>| ' |/ ._>| '_><_> | | | / . \| '_>
 `____/\___.|_|_|\___.|_|  <___| |_| \___/|_| 
-
-Questo programma genererà una password alfanumerica.
+.
 ____________________________________________________
 ";
 
             Console.WriteLine(title);
 
-
             // Start
-            Console.Write("Definisci Lunghezza Password: ");
-            //int length = Convert.ToInt32(Console.ReadLine());
+            Console.Write("How long do you want your password to be: ");
 
             int length = 0;
-            do
-            {        
-                int.TryParse(Console.ReadLine(), out length);
-                if (length > 127 || length < 7)
+            while (length < 8 || length > 127)
+            {
+                if (!int.TryParse(Console.ReadLine(), out length))
                 {
-                    Console.WriteLine("La password non ha i requisiti di sicurezza necessari: Caratteri Minimi: 8, Caratteri Massimi: 127.");
+                    Console.WriteLine("Please enter a valid number.");
+                    continue;
                 }
 
+                if (length < 8 || length > 127)
+                {
+                    Console.WriteLine("Your password doesn't have the necessary security requirements (8 min. characters, 127 max)");
+                }
                 else
                 {
-                   GeneratePassword(length);
-                   Console.WriteLine("Password generata e copiata automaticamente agli appunti"); // pw doesn't show for privacy sake, and it's already copied to clipboard
+                    Console.WriteLine("Password has been copied to clipboard"); // pw doesn't show for privacy sake, and it's already copied to clipboard
+                    ClipboardService.SetText(GeneratePassword(length)); // use textcopy package and pass the generated value in it
                 }
-
-            } while (length > 7 || length < 128);
-          
+            }
         }
+
         public static string GeneratePassword(int length)
         {
-            const string chars = "0123456789" 
-                + "abdcefghilmnopqrstuvwxyz" 
-                + "ABCDEFGHILMNOPQRSTUVWXYZ" 
-                + "#$%&'()*+,-./:;<=>?@[]^_`{|}~"; // could've used regex
+
+            const string numbers = "0123456789";
+            const string lower = "abdcefghilmnopqrstuvwxyz";
+            const string upper = "ABCDEFGHILMNOPQRSTUVWXYZ";
+            const string special = "#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 
             StringBuilder sb = new StringBuilder();
             Random randomize = new Random();
 
-            for (int i = 0; i < length; i++)
+            // Ensure at least one character from each category
+            sb.Append(numbers[randomize.Next(numbers.Length)]);
+            sb.Append(lower[randomize.Next(lower.Length)]);
+            sb.Append(upper[randomize.Next(upper.Length)]);
+            sb.Append(special[randomize.Next(special.Length)]);
+
+            string allChars = numbers + lower + upper + special;
+            // Fill the rest of the password with random characters
+            for (int i = 4; i < length; i++)
             {
-                int index = randomize.Next(chars.Length);
-                sb.Append(chars[index]);
+                int index = randomize.Next(allChars.Length);
+                sb.Append(allChars[index]);
             }
+            // Shuffle the password to ensure randomness
+            return new string(sb.ToString().OrderBy(_ => randomize.Next()).ToArray());
 
-            if (length > 127 || length < 7)
-            {
-                return "";
-            }
-
-            var generatedvalue = sb.ToString(); // convert int value to string
-            ClipboardService.SetText(generatedvalue); // use textcopy package and pass the generated value in it
-
-            return generatedvalue;
+            // var generatedvalue = sb.ToString(); // convert int value to string
+            // return generatedvalue;
         }
 
-    }  
+        public static bool IsPasswordStrong(string password)
+        {
+            return password.Length >= 8 &&
+            Regex.IsMatch(password, @"\d") && // Contains a digit
+            Regex.IsMatch(password, @"[a-z]") && // Contains a lowercase letter
+            Regex.IsMatch(password, @"[A-Z]") && // Contains an uppercase letter
+            Regex.IsMatch(password, @"[#$%&'()*+,-./:;<=>?@[\]^_`{|}~]"); // Contains a special character
+        }
+    }
 }
